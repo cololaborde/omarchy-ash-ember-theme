f1cb312a076fd4c3f39d9ff9392b5c01cde83ed9
cololaborde <cololaborde@gmail.com>
2025-11-06 22:28:53 -0300

    theme without lazy!

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "cololaborde/omarchy-ash-ember-theme",
+    name = "ash-ember",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("ash-ember")
+    end,
+  },
+}
