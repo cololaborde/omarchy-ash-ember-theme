bdc48b883b6fca98606be1d1b042c12470439ee5
cololaborde <cololaborde@gmail.com>
2025-11-06 21:53:00 -0300

    test nvim new structure

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "cololaborde/omarchy-ash-ember-theme",
+    name = "ash-ember",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ash-ember",
+    },
+  },
+}
+
